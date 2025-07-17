a03bb65ea27cd85b2e905cbdc15b3858777d883b
KANG-XUAN <kang900320@gmail.com>
2025-07-18 06:20:56 +0800

    🔨修改背景圖片引用

--- frontend/src/components/pages/IndexPage copy.vue
+++ frontend/src/components/pages/IndexPage copy.vue
@@ -94,7 +94,7 @@ onMounted(() => {
 	left: 0;
 	width: 100%;
 	height: 100%;
-	background-image: url('@/assets/images/background.png');
+	background-image: url('@/assets/images/background.webp'), url('@/assets/images/background.jpg');
 	background-size: cover;
 	background-position: center;
 	background-repeat: no-repeat;
